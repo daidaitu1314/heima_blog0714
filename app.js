379958fc6d7022ffb21afff81cba671c4942f1c7
daidaitu1314 <770104768@qq.com>
2017-07-14 10:47:48 +0800

    add index page nav-bar and footer

--- app.js
+++ app.js
@@ -3,6 +3,9 @@ var express = require('express');
 // 调用 express 得到一个 网站服务器
 var app = express();
 
+// 托管静态资源 express.static
+app.use('/node_modules', express.static('node_modules'));
+
 // 指定 express 的默认模板引擎
 app.set('view engine', 'ejs');
 // 指定 模板页面存储路径
