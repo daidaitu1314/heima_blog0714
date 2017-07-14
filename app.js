1e9e6c72013d6eff1814f0f85fb054bfc77bff63
daidaitu1314 <770104768@qq.com>
2017-07-14 11:31:53 +0800

    自动循环注册路由模块

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 // 导入 express 模块
 var express = require('express');
+var fs = require('fs');
+var path = require('path');
 // 调用 express 得到一个 网站服务器
 var app = express();
 
@@ -11,14 +13,29 @@ app.set('view engine', 'ejs');
 // 指定 模板页面存储路径
 app.set('views', './views');
 
-// 导入 indexRouter 模块
+/* // 导入 indexRouter 模块
 var indexRouter = require('./router/indexRouter.js');
 // 注册路由
 app.use(indexRouter);
 // 导入 userRouter 模块
 var userRouter = require('./router/userRouter.js');
 // 注册路由
-app.use(userRouter);
+app.use(userRouter); */
+
+
+// 由于将来会有很多的路由模块。所以，每次单独导入并注册路由，比较麻烦
+// 解决方案：读取 router 文件夹下所有的文件路径，然后使用 forEach 自动循环注册路由
+fs.readdir(path.join(__dirname, './router'), (err, filenames) => {
+  if (err) throw err;
+  // 自动循环注册路由
+  filenames.forEach(filename => {
+    // 拼接每个路由模块的路径
+    var routerPath = path.join(__dirname, './router', filename);
+    // var m = require(routerPath);
+    // app.use(m);
+    app.use(require(routerPath));
+  });
+});
 
 app.listen(3008, function () {
   console.log('http://127.0.0.1:3008');
